b5061995a5291e8f3f0da1ff79e8693c9ca0fef4
Brittany <b.hill0104@gmail.com>
2019-08-05 13:13:20 -0500

    Initial commit

---
+++ src/test/java/org/launchcode/capstoneyogapedia/CapstoneYogapediaApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.capstoneyogapedia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapstoneYogapediaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
